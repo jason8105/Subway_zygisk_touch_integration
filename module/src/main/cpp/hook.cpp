d8b9ce50deaa3d18da4c2b087273eea667fe1189
jason8105 <jasondsilva8197@gmail.com>
2026-08-08 00:10:51 +0530

    Auto update from local device: 2026-08-08 00:10:51

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -77,17 +77,7 @@ void *hack_thread(void *arg) {
         LOGW("Failed to get IL2CPP domain");
     }).detach();
 
-    auto eglHandle = dlopen("libEGL.so", RTLD_LAZY);
-    if (eglHandle) {
-        auto eglSwapBuffers = dlsym(eglHandle, "eglSwapBuffers");
-        if (eglSwapBuffers) {
-            DobbyHook((void*)eglSwapBuffers, (void*)hook_eglSwapBuffers,
-                      (void**)&old_eglSwapBuffers);
-            LOGI("eglSwapBuffers hooked");
-        }
-        dlclose(eglHandle);
-    }
-
+    // Hook eglMakeCurrent (for OpenGL ES games)
     auto eglMakeCurrent = dlsym(RTLD_DEFAULT, "eglMakeCurrent");
     if (eglMakeCurrent) {
         DobbyHook((void*)eglMakeCurrent, (void*)hook_eglMakeCurrent,
@@ -95,11 +85,12 @@ void *hack_thread(void *arg) {
         LOGI("eglMakeCurrent hooked");
     }
 
-    auto eglSwapBuffersWithDamageKHR = dlsym(RTLD_DEFAULT, "eglSwapBuffersWithDamageKHR");
-    if (eglSwapBuffersWithDamageKHR) {
-        DobbyHook((void*)eglSwapBuffersWithDamageKHR, (void*)hook_eglSwapBuffersWithDamageKHR,
-                  (void**)&old_eglSwapBuffersWithDamageKHR);
-        LOGI("eglSwapBuffersWithDamageKHR hooked");
+    // Hook vkQueuePresentKHR (for Vulkan games - this is likely what Among Us uses)
+    auto vkQueuePresentKHR = dlsym(RTLD_DEFAULT, "vkQueuePresentKHR");
+    if (vkQueuePresentKHR) {
+        DobbyHook((void*)vkQueuePresentKHR, (void*)hook_vkQueuePresentKHR,
+                  (void**)&old_vkQueuePresentKHR);
+        LOGI("vkQueuePresentKHR hooked");
     }
 
     LOGI("All hooks set up");
@@ -109,76 +100,6 @@ void *hack_thread(void *arg) {
 // ============================================================
 // HOOK FUNCTIONS
 // ============================================================
-
-EGLBoolean (*old_eglSwapBuffers)(EGLDisplay dpy, EGLSurface surface);
-EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
-    eglQuerySurface(dpy, surface, EGL_WIDTH, &glWidth);
-    eglQuerySurface(dpy, surface, EGL_HEIGHT, &glHeight);
-
-    if (!setupimg) {
-        SetupImgui();
-        setupimg = true;
-    }
-
-    ImGuiIO &io = ImGui::GetIO();
-
-    if (IL2CPP::domain != nullptr && IL2CPP::API::il2cpp_class_from_name != nullptr) {
-        auto corlib = IL2CPP::API::il2cpp_get_corlib();
-        if (corlib) {
-            auto inputClass = IL2CPP::API::il2cpp_class_from_name(corlib, "UnityEngine", "Input");
-            if (inputClass) {
-                auto touchCountMethod = IL2CPP::API::il2cpp_class_get_method_from_name(inputClass, "get_touchCount", 0);
-                auto getTouchMethod = IL2CPP::API::il2cpp_class_get_method_from_name(inputClass, "GetTouch", 1);
-                
-                if (touchCountMethod && touchCountMethod->methodPointer &&
-                    getTouchMethod && getTouchMethod->methodPointer) {
-                    
-                    typedef int (*get_touchCount_t)();
-                    typedef UnityEngine_Touch_Fields (*get_touch_t)(int);
-                    
-                    auto TouchCount = (get_touchCount_t)touchCountMethod->methodPointer;
-                    auto GetTouch = (get_touch_t)getTouchMethod->methodPointer;
-                    
-                    int touchCount = TouchCount();
-                    if (touchCount > 0) {
-                        UnityEngine_Touch_Fields touch = GetTouch(0);
-                        float reverseY = io.DisplaySize.y - touch.m_Position.fields.y;
-
-                        switch (touch.m_Phase) {
-                            case TouchPhase::Began:
-                            case TouchPhase::Stationary:
-                                io.MousePos = ImVec2(touch.m_Position.fields.x, reverseY);
-                                io.MouseDown[0] = true;
-                                break;
-                            case TouchPhase::Ended:
-                            case TouchPhase::Canceled:
-                                io.MouseDown[0] = false;
-                                break;
-                            case TouchPhase::Moved:
-                                io.MousePos = ImVec2(touch.m_Position.fields.x, reverseY);
-                                break;
-                            default:
-                                break;
-                        }
-                    } else {
-                        io.MouseDown[0] = false;
-                    }
-                }
-            }
-        }
-    }
-
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplAndroid_NewFrame();
-    ImGui::NewFrame();
-    DrawKenzGUIMenu();
-    Patches();
-    ImGui::Render();
-    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-    
-    return old_eglSwapBuffers(dpy, surface);
-}
 
 void (*old_eglMakeCurrent)(EGLDisplay dpy, EGLSurface draw, EGLSurface read, EGLContext ctx);
 void hook_eglMakeCurrent(EGLDisplay dpy, EGLSurface draw, EGLSurface read, EGLContext ctx) {
@@ -204,75 +125,20 @@ void hook_eglMakeCurrent(EGLDisplay dpy, EGLSurface draw, EGLSurface read, EGLCo
     }
 }
 
-// ============================================================
-// eglSwapBuffersWithDamageKHR hook (was missing)
-// ============================================================
-EGLBoolean (*old_eglSwapBuffersWithDamageKHR)(EGLDisplay dpy, EGLSurface surface, EGLint* rects, EGLint n_rects);
-EGLBoolean hook_eglSwapBuffersWithDamageKHR(EGLDisplay dpy, EGLSurface surface, EGLint* rects, EGLint n_rects) {
-    eglQuerySurface(dpy, surface, EGL_WIDTH, &glWidth);
-    eglQuerySurface(dpy, surface, EGL_HEIGHT, &glHeight);
-
-    if (!setupimg) {
-        SetupImgui();
-        setupimg = true;
+void (*old_vkQueuePresentKHR)(void* queue, void* pPresentInfo);
+void hook_vkQueuePresentKHR(void* queue, void* pPresentInfo) {
+    // Only render ImGui if setup is done
+    if (setupimg) {
+        ImGuiIO &io = ImGui::GetIO();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplAndroid_NewFrame();
+        ImGui::NewFrame();
+        DrawKenzGUIMenu();
+        Patches();
+        ImGui::Render();
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     }
-
-    ImGuiIO &io = ImGui::GetIO();
-
-    if (IL2CPP::domain != nullptr && IL2CPP::API::il2cpp_class_from_name != nullptr) {
-        auto corlib = IL2CPP::API::il2cpp_get_corlib();
-        if (corlib) {
-            auto inputClass = IL2CPP::API::il2cpp_class_from_name(corlib, "UnityEngine", "Input");
-            if (inputClass) {
-                auto touchCountMethod = IL2CPP::API::il2cpp_class_get_method_from_name(inputClass, "get_touchCount", 0);
-                auto getTouchMethod = IL2CPP::API::il2cpp_class_get_method_from_name(inputClass, "GetTouch", 1);
-                
-                if (touchCountMethod && touchCountMethod->methodPointer &&
-                    getTouchMethod && getTouchMethod->methodPointer) {
-                    
-                    typedef int (*get_touchCount_t)();
-                    typedef UnityEngine_Touch_Fields (*get_touch_t)(int);
-                    
-                    auto TouchCount = (get_touchCount_t)touchCountMethod->methodPointer;
-                    auto GetTouch = (get_touch_t)getTouchMethod->methodPointer;
-                    
-                    int touchCount = TouchCount();
-                    if (touchCount > 0) {
-                        UnityEngine_Touch_Fields touch = GetTouch(0);
-                        float reverseY = io.DisplaySize.y - touch.m_Position.fields.y;
-
-                        switch (touch.m_Phase) {
-                            case TouchPhase::Began:
-                            case TouchPhase::Stationary:
-                                io.MousePos = ImVec2(touch.m_Position.fields.x, reverseY);
-                                io.MouseDown[0] = true;
-                                break;
-                            case TouchPhase::Ended:
-                            case TouchPhase::Canceled:
-                                io.MouseDown[0] = false;
-                                break;
-                            case TouchPhase::Moved:
-                                io.MousePos = ImVec2(touch.m_Position.fields.x, reverseY);
-                                break;
-                            default:
-                                break;
-                        }
-                    } else {
-                        io.MouseDown[0] = false;
-                    }
-                }
-            }
-        }
-    }
-
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplAndroid_NewFrame();
-    ImGui::NewFrame();
-    DrawKenzGUIMenu();
-    Patches();
-    ImGui::Render();
-    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     
-    return old_eglSwapBuffersWithDamageKHR(dpy, surface, rects, n_rects);
+    old_vkQueuePresentKHR(queue, pPresentInfo);
 }
