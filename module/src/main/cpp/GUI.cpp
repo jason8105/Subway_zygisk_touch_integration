39f9f581775fe30903b2750f10dc8510becfbad1
jason8105 <jasondsilva8197@gmail.com>
2026-08-07 16:32:53 +0530

    Auto update from local device: 2026-08-07 16:32:53

--- module/src/main/cpp/GUI.cpp
+++ module/src/main/cpp/GUI.cpp
@@ -15,40 +15,6 @@
 extern int glWidth, glHeight;
 extern bool setupimg;
 
-struct UnityEngine_Vector2_Fields {
-    float x;
-    float y;
-};
-
-struct UnityEngine_Vector2_o {
-    UnityEngine_Vector2_Fields fields;
-};
-
-enum TouchPhase {
-    Began = 0,
-    Moved = 1,
-    Stationary = 2,
-    Ended = 3,
-    Canceled = 4
-};
-
-struct UnityEngine_Touch_Fields {
-    int32_t m_FingerId;
-    struct UnityEngine_Vector2_o m_Position;
-    struct UnityEngine_Vector2_o m_RawPosition;
-    struct UnityEngine_Vector2_o m_PositionDelta;
-    float m_TimeDelta;
-    int32_t m_TapCount;
-    int32_t m_Phase;
-    int32_t m_Type;
-    float m_Pressure;
-    float m_maximumPossiblePressure;
-    float m_Radius;
-    float m_fRadiusVariance;
-    float m_AltitudeAngle;
-    float m_AzimuthAngle;
-};
-
 static int currentTab = 0;
 
 void DrawKenzGUIMenu() {
--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE
 #include <cstring>
 #include <cstdio>
 #include <unistd.h>
